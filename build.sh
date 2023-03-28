e175c3741ccbc5214a61c7caa5095023ffb2acda
Chris McCormick <chris@mccormick.cx>
2023-03-28 20:09:14 +0800

    Initial commit.

---
+++ build.sh
@@ -0,0 +1,20 @@
+# build this repo
+git clone https://github.com/ggerganov/llama.cpp
+cd llama.cpp
+make
+
+# obtain the original LLaMA model weights and place them in ./models
+#ls ./models
+#65B 30B 13B 7B tokenizer_checklist.chk tokenizer.model
+
+# install Python dependencies
+#python3 -m pip install torch numpy sentencepiece
+
+# convert the 7B model to ggml FP16 format
+#python3 convert-pth-to-ggml.py models/7B/ 1
+
+# quantize the model to 4-bits
+#python3 quantize.py 7B
+
+# run the inference
+#./main -m ./models/7B/ggml-model-q4_0.bin -n 128
