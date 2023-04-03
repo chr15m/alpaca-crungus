400a1c14140e9def13c6264593b84f420ca7a721
Chris McCormick <chris@mccormick.cx>
2023-04-03 21:48:52 +0800

    Pass args to chat.

--- chat.sh
+++ chat.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./llama.cpp/main --color -i -ins -n 512 --threads 8 -p "You are a helpful AI who will assist, provide information, answer questions, and have conversations." -m ggml-alpaca-7b-native-q4.bin
+./llama.cpp/main --color -i -ins -n 512 --threads 8 -p "You are a helpful AI who will assist, provide information, answer questions, and have conversations." -m ggml-alpaca-7b-native-q4.bin "$@"
